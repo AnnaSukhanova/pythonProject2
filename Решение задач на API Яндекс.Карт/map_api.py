9b68d9d41356d75fa626812cd2ba8cdcc00eae1e
Anna <annasuhanova2005@gmail.com>
2022-02-19 20:53:02 +0400

    вывод карты

--- Решение задач на API Яндекс.Карт/map_api.py
+++ Решение задач на API Яндекс.Карт/map_api.py
@@ -1,13 +1,39 @@
+import os
 import sys
 
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('map_api.ui', self)
+        self.x, self.y, self.m, self.l = '37.530887', '55.70311', '0.002', 'map'
+        self.getImage()
+        self.initUI()
+
+    def getImage(self):
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.x},{self.y}&spn={self.m},{self.m}&l={self.l}"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
 
 
 if __name__ == '__main__':
